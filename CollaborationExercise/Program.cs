9c050bb96d6545c313b569fc55c918f20869770c
ru <g.e.usagi@gmail.com>
2014-10-06 13:29:02 +0200

    first

---
+++ CollaborationExercise/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborationExercise
+{
+    class Program
+    {
+
+        /// <summary>
+        /// this is the main method
+        /// it recieves an input from command line 
+        /// and checks if it's numbers or letters
+        /// it will then check if it is a palindrome
+        /// if it is a number it will present the same number in binary and roman numbers 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+        }
+        /// <summary>
+        /// return true if input is a palindrome
+        /// else false
+        /// </summary>
+        /// <param name="text">text input to be validated</param>
+        /// <returns></returns>
+    private static bool IsPalindrome (string text)
+        {
+            
+        }
+
+        private static bool ContainsOnlyDigits(string text)
+        {
+            
+        }
+
+        private static string ConvertToBinaryNumbers(int input)
+        {
+            
+        }
+
+        private static string ConvertToRomanNumbers(int input)
+        {
+            
+        }
+     }
+}
