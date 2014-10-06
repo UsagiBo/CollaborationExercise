cf5faec3913fd917cda8d3e9f7c487e1b06c9964
mamoorlook <mamoorlook@gmail.com>
2014-10-06 14:27:32 +0200

    Palindrome

--- CollaborationExercise/Program.cs
+++ CollaborationExercise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,34 +9,64 @@ namespace CollaborationExercise
 {
     class Program
     {
-
-        /// <summary>
-        /// this is the main method
-        /// it recieves an input from command line 
-        /// and checks if it's numbers or letters
-        /// it will then check if it is a palindrome
-        /// if it is a number it will present the same number in binary and roman numbers 
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
+            string initial = "";
+
+            Console.WriteLine("Enter text to check");
+
+            string text = Console.ReadLine();
+
+            int iLength = text.Length;
+
+            if (iLength == 0)
+            {
+                Console.WriteLine("You did not enter any text");
+
+            }
+
+            else
+            {
+
+                for (int j = iLength - 1; j >= 0; j--)
+                {
+                    initial = initial + text[j];
+                }
+
+                if (initial == text)
+                {
+                    Console.WriteLine(text + " is palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(text + " is not a palindrome");
+                }
+
+
+                Console.Read();
+            }
         }
+
         /// <summary>
         /// return true if input is a palindrome
         /// else false
         /// </summary>
         /// <param name="text">text input to be validated</param>
         /// <returns></returns>
-    private static bool IsPalindrome (string text)
+        /*private static bool IsPalindrome (string text)
         {
             
         }
 
-        private static bool ContainsOnlyDigits(string text)
+        /*private static bool ContainsOnlyDigits(string text)
         {
-            
+            bool line;
+            double retNum;
+
+            line = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            return line;
         }
+        /*
 
         private static string ConvertToBinaryNumbers(int input)
         {
@@ -45,6 +76,6 @@ namespace CollaborationExercise
         private static string ConvertToRomanNumbers(int input)
         {
             
-        }
+        }*/
      }
 }
